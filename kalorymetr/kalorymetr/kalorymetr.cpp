645215d25e5529895e2acc3448837aacbcf47482
necki <dawidk089@o2.pl>
2015-03-28 23:38:56 +0100

    Revert "okreslenie funkcji"
    This reverts commit b48770f04907adc7977bb76572c7a8ae7058db21.

--- kalorymetr/kalorymetr/kalorymetr.cpp
+++
@@ -1,6 +0,0 @@
-/*
- * CPPFile1.cpp
- *
- * Created: 2015-03-26 23:54:46
- *  Author: mcmushroom
- */ 
--- kalorymetr/kalorymetr/modules/ATMEGA.h
+++
@@ -1,6 +0,0 @@
-/*
- * ATMEGA.h
- *
- * Created: 2015-03-26 16:02:58
- *  Author: mcmushroom
- */ 
